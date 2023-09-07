fa9a018b2a6c41cf61253a164de9df082d390488
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-07 21:23:28 +0300

    Add files via upload

--- ObjectDescriptor.js
+++ ObjectDescriptor.js
@@ -1,18 +1,30 @@
 
 const car = {
     brand: 'BrandName',
-    year: 2023
+    year: 2023,
+
+    get age(){
+        return `Car was released in ${this.year}`;
+    },
+
+    set age(value){
+        this.year = value;
+    }
 }
 
-Object.defineProperty(car, 'year', {
+//Object.defineProperty(car, 'age', {
     //configurable: false,
-    enumerable: false
-});
+    //enumerable: false
+    //writable: false,
 
-console.log(Object.keys(car));
-console.log(car.propertyIsEnumerable('year'));
-
-
+//    get: function(){
+//        return `Car was released in ${this.year}`;
+//    }
+//});
+//console.log(Object.keys(car));
+//console.log(car.propertyIsEnumerable('year'));
+car.age = 2020;
+console.log(car.age);
 
 
 
