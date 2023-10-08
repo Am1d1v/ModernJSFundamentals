4bbea5f11e11707ca258fb1b3e1dc6ead2b5d746
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-08 17:24:03 +0300

    Add files via upload

--- closure.js
+++ closure.js
@@ -12,6 +12,7 @@ function Minus(num1 = 0){
 console.log(Minus()(6));
 */
 
+/*
 function Multiply(num1){
 
     return function(num2){
@@ -23,3 +24,35 @@ function Multiply(num1){
 const mult = Multiply(10);
 console.log(mult(2));
 console.log(mult(3));
+*/
+
+const module = (function(){
+
+    let str = '';
+
+    function setString(value = ''){
+
+        str = String(value);
+        return str;
+    }
+
+    function GetString(){
+        return str;
+    }
+
+    function StringLength(){
+        return str.length;
+    }
+
+    function ReverseString(){
+        return str.split('').reverse().join('');
+    }
+
+    return {
+        setString,
+        GetString,
+        StringLength,
+        ReverseString
+    }
+})();
+
